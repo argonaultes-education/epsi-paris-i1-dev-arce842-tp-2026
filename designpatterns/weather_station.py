46a1f9efc5863f6b7f324391be7f21c7ea999798
argonaultes <gael@argonaultes.fr>
2026-03-06 11:26:02 +0100

    rename with observer and caches values

--- designpatterns/weather_station.py
+++ designpatterns/weather_station.py
@@ -2,19 +2,19 @@ import random
 import datetime
 from abc import ABC, abstractmethod
 
-class AbstractDisplay(ABC):
+class Observer(ABC):
     
     @abstractmethod
-    def display(self, temperature, pressure, humidy):
+    def notify(self, weather_station):
         ...
     
 
-class StatsDisplay(AbstractDisplay):
+class StatsDisplay(Observer):
     
-    def display(self, temperature, pressure, humidity):
-        print(f'{temperature} | {pressure} | {humidity}')
+    def notify(self, weather_station):
+        print(f'{weather_station.temperature} | {weather_station.pressure} | {weather_station.humidity}')
         
-class AverageTempDisplay(AbstractDisplay):
+class AverageTempDisplay(Observer):
     
     def __init__(self):
         self.__accu_temp = 0
@@ -25,15 +25,16 @@ class AverageTempDisplay(AbstractDisplay):
         self.__nb_temp += 1
         print(f'avg temp {self.__accu_temp / self.__nb_temp}')
         
-    def display(self, temperature, pressure, humidity):
-        self.average_temp(temperature)
+    def notify(self, weather_station):
+        self.average_temp(weather_station.temperature)
 
-class MaxPressureDisplay(AbstractDisplay):
+class MaxPressureDisplay(Observer):
     
     def __init__(self):
         self.__max_pressure = 0
         
-    def display(self, temperature, pressure, humidity):
+    def notify(self, weather_station):
+        pressure = weather_station.pressure
         if self.__max_pressure < pressure:
             self.__max_pressure = pressure
         print(f'Max pressure: {self.__max_pressure}')
@@ -43,29 +44,47 @@ class WeatherStation:
     def __init__(self):
         self.__displays = []
         
-    def add_display(self, display:AbstractDisplay):
-        if isinstance(display, AbstractDisplay):
+    def add_display(self, display:Observer):
+        if isinstance(display, Observer):
             self.__displays.append(display)
     
-    def get_temperature(self):
+    def __get_temperature(self):
         return random.randint(-50, 50)
     
-    def get_pressure(self):
+    def __get_pressure(self):
         return random.randint(800, 1200)
     
-    def get_humidity(self):
+    def __get_humidity(self):
         return random.randint(0, 100)
     
-    def get_pm(self):
+    def __get_pm(self):
         return random.randint(0, 1000)
     
+    @property
+    def temperature(self):
+        return self.__current_temp
+    
+    @property
+    def pressure(self):
+        return self.__current_pressure
+
+    @property
+    def humidity(self):
+        return self.__current_humidity
+
+    @property
+    def pm(self):
+        return self.__current_pm
+
+    
     def update_measures(self):
-        current_temp = self.get_temperature()
-        current_pressure = self.get_pressure()
-        current_humidity = self.get_humidity()
+        self.__current_temp = self.__get_temperature()
+        self.__current_pressure = self.__get_pressure()
+        self.__current_humidity = self.__get_humidity()
+        self.__current_pm = self.__get_pm()
 
         for display in self.__displays:
-            display.display(current_temp, current_pressure, current_humidity)
+            display.notify(self)
         
 if __name__ == '__main__':
     weather_station = WeatherStation()
@@ -73,6 +92,6 @@ if __name__ == '__main__':
     weather_station.add_display(AverageTempDisplay())
     weather_station.add_display(MaxPressureDisplay())
     
-    for _ in range(10):
+    for _ in range(1):
         print(datetime.datetime.now())
         weather_station.update_measures()
