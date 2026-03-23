179a28369b4927cdce12073bd2bacc5b699b0e15
argonaultes <gael@argonaultes.fr>
2026-03-23 12:30:40 +0100

    decorator python

--- designpatterns/decorator_test.py
+++ designpatterns/decorator_test.py
@@ -1,6 +1,13 @@
+def give_me_stats(func):
+    def inner_func():
+        print('before')
+        func()
+        print('after')
+    return inner_func
+
+@give_me_stats
 def display_hello():
     print('hello')
-    
 
 display_hello = give_me_stats(display_hello)
 
